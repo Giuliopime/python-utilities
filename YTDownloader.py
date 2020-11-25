f057109512397dc564704120ca90bfc8840743fb
Giuliopime <giuliopime@gmail.com>
2020-11-25 22:36:31 +0100

    Done conversion to mp3

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,24 +1,24 @@
-# import os
+import os
+import time
 from pytube import YouTube
-# from moviepy.editor import *
+from moviepy.editor import *
 
 ytd = YouTube(input("Please input the yt link\n"))
 
 dlType = ""
-while dlType != "audio" and dlType != "video":
-    dlType = input("Insert\n- 'audio' to download only audio\n- 'video' to download the full video\n").lower()
+while dlType != "audio" and dlType != "video" and dlType != "1" and dlType != "2":
+    dlType = input("Insert\n- 'audio' or '1' to download only audio\n- 'video' or '2' to download the full video\n").lower()
 
-stream = ytd.streams.filter(only_audio=(True if dlType == "audio" else False))
+stream = ytd.streams
 
 print("Downloading...")
 
-filePath = stream[0].download(".")
-
-"""
-if dlType == "audio":
+if dlType == "audio" or dlType == "1":
+    filePath = stream[0].download(".")
     video = VideoFileClip(filePath)
-    video.audio.write_audiofile("./", ytd.title)
+    video.audio.write_audiofile("./" + ytd.title + ".mp3")
+    video.close()
     os.remove(filePath)
-"""
+
 
 print("Successfully downloaded yt video")
